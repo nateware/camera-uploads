453075201c7489cbafb9b471141f32c13cc9cb26
nateware <nwiger@gmail.com>
2015-04-14 13:04:34 -0700

    support photos list and bg log

--- rename-photos.rb
+++ rename-photos.rb
@@ -10,11 +10,20 @@ require 'date'
 require 'fileutils'
 
 # Where the photos are and where they're going.
-PHOTO_DIR = ENV['PHOTO_DIR'] || '/Volumes/Fatman/Dropbox/Photos'
+PHOTO_DIR  = ENV['PHOTO_DIR']  || "#{ENV['HOME']}/Pictures"
+UPLOAD_DIR = ENV['UPLOAD_DIR'] || "#{PHOTO_DIR}/Incoming"
+FLASH_CARD = '/Volumes/CAMERA*'
 
 # The format for the new file names.
 DATE_SUBDIR = "%Y/%Y-%m-%d"
-FILE_FORMAT = "%Y-%m-%d %H:%M:%S" # strtime
+FILE_FORMAT = "%Y-%m-%d %H.%M.%S" # strtime
+
+# Log if run from a script
+unless STDOUT.isatty
+  fh = File.open('/tmp/camera-uploads.log','a')
+  STDOUT.reopen(fh)
+  STDERR.reopen(fh)
+end
 
 # Return the date/time on which the given photo was taken.
 def photo_date(file)
@@ -39,16 +48,25 @@ end
 # Sanity check
 abort "Missing image dir: #{PHOTO_DIR}" unless File.directory?(PHOTO_DIR)
 
+# Figure out if we were given a list of photos on the command line
+photos =
+  if ARGV.length > 0
+    ARGV
+  else
+    Dir["#{UPLOAD_DIR}/*"]
+  end
+
 # Copy photos into year and month subfolders. Name the copies according to
 # their timestamps. If more than one photo has the same timestamp, add
 # suffixes 'a', 'b', etc. to the names.
-for photo in ARGV
+for photo in photos
   unless File.file?(photo)
     puts "File does not exist: #{photo}"
     next
   end
 
   date = photo_date(photo)
+  basename  = File.basename(photo)
   extension = File.extname(photo)
   puts "Processing #{photo} (#{extension}) from #{date}" if ENV['DEBUG']
 
@@ -69,4 +87,14 @@ for photo in ARGV
   puts "#{photo} -> #{newpath}"
   abort "File exists somehow: #{newpath}" if File.exists?(newpath)
   FileUtils.mv(photo, newpath) unless ENV['TEST']
+
+  # Delete from flash card
+  # NO: This throws off the Image Capture app
+  # sources = Dir["#{FLASH_CARD}/**/#{basename}"]
+  # unless sources.length == 1
+  #   abort "Expecting exactly one source for #{basename}: #{sources}"
+  # end
+  # orig = sources.first
+  # puts "rm #{orig}"
+  # File.unlink(orig)
 end
